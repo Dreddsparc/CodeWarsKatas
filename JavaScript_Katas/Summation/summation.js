b1df716f3d63bf72ab99bcc641e7354dbbaa1ebd
Christian Weaver <sparc399@gmail.com>
2023-02-27 07:41:25 -0600

    added chai and mocha for tests, update .gitignore

---
+++ JavaScript_Katas/Summation/summation.js
@@ -0,0 +1,27 @@
+/*
+Summation
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+For example (Input -> Output):
+
+2 -> 3 (1 + 2)
+8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+CodeWars Kata Link:
+https://www.codewars.com/kata/55d24f55d7dd296eb9000030/javascript
+
+ */
+
+var summation = function (num) {
+    // Code here
+}
+
+const assert = require('chai').assert;
+
+describe('summation', function () {
+    it('should return the correct total', function () {
+        assert.strictEqual(summation(1),  1);
+        assert.strictEqual(summation(2),  3);
+        assert.strictEqual(summation(8), 36);
+    })
+})
