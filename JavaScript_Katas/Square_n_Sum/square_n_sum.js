dd302e96519893c8b9c84d669249bd0496d38db3
Christian Weaver <sparc399@gmail.com>
2023-02-27 06:36:51 -0600

    moved the javascript katas to their own directory added README

--- JavaScript_Katas/Square_n_Sum/square_n_sum.js
+++ JavaScript_Katas/Square_n_Sum/square_n_sum.js
@@ -8,15 +8,11 @@ For example, for [1, 2, 2] it should return 9 because
 * */
 function squareSum(numbers){
     let sumSquares = 0;
-    let debug = true;
-    if(debug){ console.log(numbers);}
     for (let i = 0; i < numbers.length; i++) {
-        if (debug) { console.log(numbers[i]);}
         sumSquares += numbers[i]**2;
-
     }
     return sumSquares;
 }
 
-
+// Local Testing Only Below
 console.log(squareSum([1, 2, 7, 19, 11, 22, 43]));
