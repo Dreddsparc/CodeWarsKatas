f3b1d2444cdf2a489df8b7f3aff2673e49895471
Christian Weaver <sparc399@gmail.com>
2023-02-27 08:56:50 -0600

    Added Python lang directory, and a few past katas

---
+++ Python/multiply/multiply.py
@@ -0,0 +1,10 @@
+"""
+  This code does not execute properly, try to figure out why.
+
+CodeWars Kata Link :
+https://www.codewars.com/kata/50654ddff44f800200000004/train/javascript
+"""
+def multiply(a, b):
+    return a * b
+
+# Code for local testing beyond this line
